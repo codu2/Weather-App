93de3733c957b60c8878cb61fcff7aac64b01868
codu2 <82283517+codu2@users.noreply.github.com>
2021-08-30 02:26:05 +0900

    update main.js
    To update its current, lowest, highest temperature, and weather conditions at searched city using The OpenWeather API.

--- main.js
+++ main.js
@@ -10,7 +10,18 @@ const spitOutCelcius = (kelvin) => {
     return celcius;
 }
 
+const isDayTime = (icon) => {
+    if (icon.includes('d')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 updateWeatherApp = (city) => {
+    console.log(city);
+    const imageName = city.weather[0].icon;
+    const iconSrc = `http://openweathermap.org/img/wn/${imageName}@2x.png`
     cityName.textContent = city.name;
     cardBody.innerHTML = `
     <div class="card-mid">
@@ -25,7 +36,7 @@ updateWeatherApp = (city) => {
                 </div>
 
                 <div class="icon-container">
-                    <i class="ri-cloud-line"></i>
+                    <img src="${iconSrc}" alt="" />
                 </div>
                 <div class="card-bottom">
                     <div class="desc">
@@ -33,11 +44,16 @@ updateWeatherApp = (city) => {
                         <span>Feels Like</span>
                     </div>
                     <div class="desc">
-                        <p>${spitOutCelcius(city.main.humidity)}%</p>
+                        <p>${city.main.humidity}%</p>
                         <span>Humidity</span>
                     </div>
                 </div>
-    `
+    `;
+    if (isDayTime(imageName)) {
+        timeImage.style.backgroundImage = "url('/day.jpeg')";
+    } else {
+        timeImage.style.backgroundImage = "url('/night.jpeg')";
+    }
 }
 
 
@@ -53,3 +69,40 @@ searchForm.addEventListener('submit', e => {
         })
         .catch((error) => { console.log(error) });
 })
+
+const today = document.querySelector('.today');
+const date = document.querySelector('.date');
+
+const now = new Date();
+
+date.innerHTML = now.toUTCString();
+
+/*
+function getNow() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+    const day = today.getDay();
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const hours = today.getHours();
+    const minutes = today.getMinutes();
+    const seconds = today.getSeconds();
+
+    date.innerHTML = `<div class="date">
+                    ${year} ${addZero(month)} ${addZero(date)} ${days[day]}
+                    ${addZero(hours)} : ${addZero(minutes)} : ${addZero(seconds)}
+                    </div>`;
+}
+
+function addZero(num) {
+    num < 10 ? `0` + num : num;
+}
+
+function init() {
+    getNow();
+    setInterval(getNow(), 1000);
+}
+
+init();
+*/
